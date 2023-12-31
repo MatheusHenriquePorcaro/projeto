e658c2c115fb620f8b0519236cd6c01fb71d2317
MatheusHenriquePorcaro <math3usporcaro@gmail.com>
2023-12-31 17:03:35 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  /*if (html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }*/
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("alt", "Foto de pessoa de camisa preta e óculos sorrindo com fundo azul e roxo")
+  }
+  else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto de pessoa de camisa preta e óculos sorrindo com fundo amarelo.")
+  }
+}
